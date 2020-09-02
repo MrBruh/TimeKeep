f57a6e863e67dd5a335bfdbe7dd687e87bd4f389
MrBruh <mdnssv@gmail.com>
2020-09-02 00:58:06 -0400

    Added a testing button

--- gtk_test.py
+++ gtk_test.py
@@ -4,14 +4,27 @@
 #
 # Michael Denissov 2020
 #
-# All this does is tests that everything is working
+# All this does is tests that everything is working, with a button now
 
 import gi
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-win = Gtk.Window()
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Hello World")
+
+        self.button = Gtk.Button(label="Click Here")
+        self.button.connect("clicked", self.on_button_clicked)
+        self.add(self.button)
+
+    def on_button_clicked(self, widget):
+        print("Hello World")
+
+
+win = MyWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
