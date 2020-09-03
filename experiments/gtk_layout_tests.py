507887a630b1c9d313a21e4852b7b70171d07d69
MrBruh <mdnssv@gmail.com>
2020-09-03 01:02:52 -0400

    playing around with layouts

--- experiments/gtk_layout_tests.py
+++ experiments/gtk_layout_tests.py
@@ -9,29 +9,29 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 
-class MyWindow(Gtk.Window):
+class GridWindow(Gtk.Window):
     def __init__(self):
-        Gtk.Window.__init__(self, title="Hello World")
+        Gtk.Window.__init__(self, title="Grid Example")
 
-        self.box = Gtk.Box(spacing=6)
-        self.add(self.box)
+        grid = Gtk.Grid()
+        self.add(grid)
 
-        self.button1 = Gtk.Button(label="Hello")
-        self.button1.connect("clicked", self.on_button1_clicked)
-        self.box.pack_start(self.button1, True, True, 0)
+        button1 = Gtk.Button(label="Button 1")
+        button2 = Gtk.Button(label="Button 2")
+        button3 = Gtk.Button(label="Button 3")
+        button4 = Gtk.Button(label="Button 4")
+        button5 = Gtk.Button(label="Button 5")
+        button6 = Gtk.Button(label="Button 6")
 
-        self.button2 = Gtk.Button(label="Goodbye")
-        self.button2.connect("clicked", self.on_button2_clicked)
-        self.box.pack_start(self.button2, True, True, 0)
-
-    def on_button1_clicked(self, widget):
-        print("Hello")
-
-    def on_button2_clicked(self, widget):
-        print("Goodbye")
+        grid.add(button1)
+        grid.attach(button2, 1, 0, 2, 1)
+        grid.attach_next_to(button3, button1, Gtk.PositionType.BOTTOM, 1, 2)
+        grid.attach_next_to(button4, button3, Gtk.PositionType.RIGHT, 2, 1)
+        grid.attach(button5, 1, 2, 1, 1)
+        grid.attach_next_to(button6, button5, Gtk.PositionType.RIGHT, 1, 1)
 
 
-win = MyWindow()
+win = GridWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
